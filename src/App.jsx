c36180b37775ce081c942388890dd6b2b0e30047
Arunjith.A <arunjith1749@gmail.com>
2023-06-03 09:59:15 +0530

    error page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
 import AdminLogin from './components/Admin/Login/AdminLogin'
 import AdminHome from './components/Admin/AdminHome/AdminHome'
 import Modals from './components/Admin/modal/Modals'
+import ErrorPage from './components/Admin/ErrorPage/ErrorPage'
 
 
 
@@ -15,6 +16,7 @@ function App() {
    <Toaster/>
    <Router>
      <Routes>
+     <Route path = '*' element ={<ErrorPage/>}></Route>
      <Route path = '/' element ={<AdminLogin/>}></Route>
       <Route path = '/admin_home' element = {<AdminHome/>}></Route>
       <Route path='/modal' element={<Modals/>}></Route>
